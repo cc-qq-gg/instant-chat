99af69ad998164244c53bca80a6c8f7e245ce19c
ChenQian <1736014720@qq.com>
2021-11-07 16:46:11 +0800

    9.设置默认命令行参数并解析

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -28,7 +29,20 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+// 每个go文件中都有init函数
+var serverIp string
+var serverPort int
+
+// 通过命令行设置ip和端口号
+// ./client -h，可以设置的默认信息
+func init() {
+	flag.StringVar(&serverIp, "ip", "192.168.1.13", "设置服务器IP地址（默认192.168.1.13）")
+	flag.IntVar(&serverPort, "port", 8888, "设置端口好（默认888）")
+}
 func main() {
+	// 解析命令行
+	flag.Parse()
+
 	client := NewClient("192.168.1.13", 8888)
 	if client == nil {
 		fmt.Println(">>>>连接失败")
